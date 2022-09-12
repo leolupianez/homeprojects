e6cc7089f98298a3d15d3d2f12f6673b34b67cef
Jose Lupianez <joselupianez@fullsail.edu>
2022-09-11 20:00:01 -0400

    Added EJS and index page route.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const app = express();
-const logger = require('morgan');
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+const mainRoutes = require('./routes/main')
 
 // Load environment variables
 require('dotenv').config({ path: "./config/.env" })
+
+//Using EJS for views
+app.set("view engine", "ejs");
 
 //Static Folder
 app.use(express.static("public"));
 
 // Logging
 app.use(logger('dev'))
+
+//Setup Routes
+app.use("/", mainRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}.`)
