a3c0fd29a65aaa90813d16652951ef1d9150ef25
Jose Lupianez <joselupianez@fullsail.edu>
2022-10-03 10:00:42 -0400

    ISSUE-21: Add Hero to Index Pages and Improve Views

--- routes/homeowner/projects.js
+++ routes/homeowner/projects.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const upload = require("../../middleware/multer")
-const {ensureHomeowner, ensureAuth, ensureGuest} = require('../../middleware/auth')
+const {ensureHomeowner, ensureAuth} = require('../../middleware/auth')
 const projectsController = require('../../controllers/homeowner/projects')
 
 router.get('/', ensureAuth, ensureHomeowner, projectsController.getProjects)
-router.get('/:id', ensureAuth, ensureHomeowner, projectsController.getProject)
 router.get('/add', ensureAuth, ensureHomeowner, projectsController.getAdd)
 router.post('/add', ensureAuth, ensureHomeowner, upload.array('photos', 10), projectsController.postAdd)
+router.get('/:id', ensureAuth, ensureHomeowner, projectsController.getProject)
 
 module.exports = router;
