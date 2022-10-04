2fe041f3504b02fb27e043c591775ad540ad050d
Jose Lupianez <joselupianez@fullsail.edu>
2022-10-04 08:51:07 -0400

    ISSUE-26: Add Edit Button to Project

--- controllers/homeowner/projects.js
+++ controllers/homeowner/projects.js
@@ -62,15 +62,6 @@ module.exports = {
             errors.push({msg: 'Invalid zipcode.'})
         }
 
-        let photos = []
-        if(req.files){
-            for (let i = 0; i < req.files.length; i++) {
-                // Upload image to cloudinary
-                const result = await cloudinary.uploader.upload(req.files[i].path)
-                photos.push(result.secure_url)
-            }
-        }
-
         if (Object.values(validationErrors).indexOf(true) > -1) {
             if(errors.length > 0) req.flash("errors", errors)
             res.render('homeowner/projects/add', {
@@ -84,6 +75,15 @@ module.exports = {
         }else {
             // Successful validation
             try {
+                let photos = []
+                if(req.files){
+                    for (let i = 0; i < req.files.length; i++) {
+                        // Upload image to cloudinary
+                        const result = await cloudinary.uploader.upload(req.files[i].path)
+                        photos.push(result.secure_url)
+                    }
+                }
+
                 const project = new Project({user: req.user.id, title, category, description, zipCode, images: photos})
                 project.save(err => {
                     if(err){
@@ -111,6 +111,94 @@ module.exports = {
             }
         } catch (err){
             console.error(err)
+        }
+    },
+    editProject: async (req, res, next) => {
+        try {
+            const project = await Project.findById(req.params.id).lean()
+            if(project.user != req.user.id || !project) {
+                res.redirect('/projects')
+            }else{
+                res.render('homeowner/projects/edit', {
+                    id: project._id,
+                    category: project.category,
+                    title: project.title,
+                    description: project.description,
+                    zipCode: project.zipCode,
+                    isLoggedIn: req.isAuthenticated()
+                })
+            }
+        } catch (err){
+            console.error(err)
+        }
+    },
+    updateProject: async (req, res, next) => {
+        try {
+            const project = await Project.findById(req.params.id)
+            if(project.user.toString() != req.user.id || !project) {
+                res.redirect('/projects')
+            }else{
+                const validationErrors = {categoryError: false, titleError: false, descriptionError: false, zipCodeError: false}
+                const { category, title, description, zipCode } = req.body
+                let errors = []
+                
+                // Invalid category
+                const allowedCategories = ['air conditioning', 'plumbing', 'roofing', 'painting', 'landscape', 'electrical', 'cleaning', 'handyman']
+                if (!category || !validator.isIn(category, allowedCategories) ){
+                    validationErrors.categoryError = true
+                    errors.push({msg: 'Invalid category.'})
+                }
+                // Title is empty or too long
+                if (!validator.isLength(title.trim(), {min: 5, max: 120})){
+                    validationErrors.titleError = true
+                    errors.push({msg: 'Project title needs to be between 5 to 120 characters.'})
+                }
+                // Description is empty
+                if (!validator.isLength(description.trim(), {min: 5})){
+                    validationErrors.descriptionError = true
+                    errors.push({msg: 'Project description is required.'})
+                }
+                // Invalid zip code
+                if(!zipcodes.lookup(zipCode)){
+                    validationErrors.zipCodeError = true
+                    errors.push({msg: 'Invalid zipcode.'})
+                }
+
+                if (Object.values(validationErrors).indexOf(true) > -1) {
+                    if(errors.length > 0) req.flash("errors", errors)
+                    res.render('homeowner/projects/edit', {
+                        validationErrors,
+                        id: project._id,
+                        category,
+                        title,
+                        description,
+                        zipCode,
+                        isLoggedIn: req.isAuthenticated()
+                    })
+                }else {
+                    // Successful validation
+                    let photos = []
+                    if(req.files){
+                        for (let i = 0; i < req.files.length; i++) {
+                            // Upload image to cloudinary
+                            const result = await cloudinary.uploader.upload(req.files[i].path)
+                            photos.push(result.secure_url)
+                        }
+                    }
+                    
+                    project.title = title
+                    project.category = category
+                    project.description = description
+                    project.zipCode = zipCode
+                    project.images = photos.length > 0 ? photos : project.images
+                    await project.save()
+
+                    req.flash("success", { msg: "Success! Your project was updated." });
+                    res.redirect(`/projects/${req.params.id}`)
+                }
+            }
+        } catch (err){
+            console.log(err)
         }
     }
 }
