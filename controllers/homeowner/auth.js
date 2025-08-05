eafdb0ed30f9f82e4de9dacea98c2bfb5ea53829
leolupianez <4380557+leolupianez@users.noreply.github.com>
2025-08-05 08:11:00 -0400

    Fix demo login

--- controllers/homeowner/auth.js
+++ controllers/homeowner/auth.js
@@ -1,87 +1,82 @@
 const passport = require("passport");
-const validator = require("validator")
-const User = require('../../models/User')
+const validator = require("validator");
+const User = require("../../models/User");
 require("dotenv").config({ path: "./config/.env" });
 
 module.exports = {
     getRegister: (req, res) => {
-        res.render("homeowner/register", {validationErrors: false})
+        res.render("homeowner/register", { validationErrors: false });
     },
-    postRegister: (req, res, next) => {    
+    postRegister: (req, res, next) => {
         const { firstName, lastName, email, password, confirmPassword } = req.body;
-        const validationErrors = {emailTakenError: false, emailError: false, passwordError: false, confirmError: false};
-        
+        const validationErrors = { emailTakenError: false, emailError: false, passwordError: false, confirmError: false };
+
         // Not a valid email
-        if (!validator.isEmail(email))
-            validationErrors.emailError = true;
+        if (!validator.isEmail(email)) validationErrors.emailError = true;
         // Password is not at least 6 characters
-        if (!validator.isLength(password, { min:6 }))
-            validationErrors.passwordError = true;
+        if (!validator.isLength(password, { min: 6 })) validationErrors.passwordError = true;
         // Does password match the password confirmation
-        if (!validator.equals(confirmPassword, req.body.password))
-            validationErrors.confirmError = true;
-        
+        if (!validator.equals(confirmPassword, req.body.password)) validationErrors.confirmError = true;
+
         if (Object.values(validationErrors).indexOf(true) > -1) {
-            res.render('homeowner/register', {
+            res.render("homeowner/register", {
                 validationErrors,
                 firstName,
-                lastName, 
-                email, 
+                lastName,
+                email,
                 password,
-                confirmPassword, 
+                confirmPassword,
             });
-        }else {
-            const user = new User({firstName, lastName, email, password})
-            User.findOne({email: email}, (err, existingUser) => {
-                if(err){
-                    return next(err)
+        } else {
+            const user = new User({ firstName, lastName, email, password });
+            User.findOne({ email: email }, (err, existingUser) => {
+                if (err) {
+                    return next(err);
                 }
-                if(existingUser){
+                if (existingUser) {
                     validationErrors.emailTakenError = true;
-                    res.render('homeowner/register', {
+                    res.render("homeowner/register", {
                         validationErrors,
                         firstName,
-                        lastName, 
-                        email, 
+                        lastName,
+                        email,
                         password,
-                        confirmPassword, 
+                        confirmPassword,
                     });
-                }else{
-                    user.save(err => {
-                        if(err){
-                            return next(err)
+                } else {
+                    user.save((err) => {
+                        if (err) {
+                            return next(err);
                         }
                         req.logIn(user, (err) => {
                             if (err) {
                                 return next(err);
                             }
-                            res.redirect('/');
-                        })
-                    })
+                            res.redirect("/");
+                        });
+                    });
                 }
-            })
+            });
         }
     },
     getLogin: (req, res) => {
-        res.render("homeowner/login", {validationErrors: false})
+        res.render("homeowner/login", { validationErrors: false });
     },
     postLogin: (req, res, next) => {
-        const validationErrors = {emailError: false, passwordError: false}
-        const { email, password} = req.body
+        const validationErrors = { emailError: false, passwordError: false };
+        const { email, password } = req.body;
 
         // Not a valid email
-        if (!validator.isEmail(email))
-            validationErrors.emailError = true;
+        if (!validator.isEmail(email)) validationErrors.emailError = true;
         // Password is not at least 6 characters
-        if (!validator.isLength(password, { min:6 }))
-            validationErrors.passwordError = true;
+        if (!validator.isLength(password, { min: 6 })) validationErrors.passwordError = true;
 
         if (Object.values(validationErrors).indexOf(true) > -1) {
-            res.render('homeowner/login', {
+            res.render("homeowner/login", {
                 validationErrors,
-                email: req.body.email
-            })
-        }else {
+                email: req.body.email,
+            });
+        } else {
             passport.authenticate("local", (err, user, info) => {
                 if (err) {
                     return next(err);
@@ -104,31 +99,37 @@ module.exports = {
         req.body.email = process.env.DEMO_USER_EMAIL;
         req.body.password = process.env.DEMO_USER_PASS;
 
-        passport.authenticate('local', (err, user, info) => {
-            if (err) { return next(err); }
+        passport.authenticate("local", (err, user, info) => {
+            if (err) {
+                return next(err);
+            }
             if (!user) {
-                req.flash('errors', info);
-                return res.redirect('/login');
+                req.flash("errors", info);
+                return res.redirect("/login");
             }
             req.logIn(user, (err) => {
-                if (err) { return next(err); }
-                req.flash('success', { msg: 'Success! You are logged in as demo user.' });
-                res.redirect(req.session.returnTo || '/');
+                if (err) {
+                    return next(err);
+                }
+                req.flash("success", { msg: "Success! You are logged in as demo user." });
+                res.redirect(req.session.returnTo || "/");
             });
         })(req, res, next);
     },
     getLogout: (req, res, next) => {
-        req.logout( err => {
-            if (err) { return next(err) }
-        })
-        req.session.user = null
+        req.logout((err) => {
+            if (err) {
+                return next(err);
+            }
+        });
+        req.session.user = null;
         req.session.save(function (err) {
-          if (err) next(err)
+            if (err) next(err);
 
-          req.session.regenerate(function (err) {
-            if (err) next(err)
-            res.redirect('/')
-          })
-        })
+            req.session.regenerate(function (err) {
+                if (err) next(err);
+                res.redirect("/");
+            });
+        });
     },
-}
+};
